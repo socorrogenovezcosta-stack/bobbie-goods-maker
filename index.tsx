41c08ecca222affa436d201c78dc7e123788a9d5
socorrogenovezcosta-stack <socorrogenovezcosta@gmail.com>
2025-12-08 23:03:32 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // Opcional, caso você crie um arquivo CSS global, mas não vai quebrar se não tiver
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Erro Crítico: Elemento 'root' não encontrado no HTML.");
+}
